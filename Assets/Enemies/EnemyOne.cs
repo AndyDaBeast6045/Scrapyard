a51a068ce5677dee7e54366e79f74fdb15d1c630
aryashah5000 <aryashah5000@gmail.com>
2025-04-09 21:05:58 -0400

    Enemy One Script and Patrol
    Enemy One Script, Eshaan will have to go into the MeleeEnemy script to configure the health. I left notes

---
+++ Assets/Enemies/EnemyOne.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class EnemyOne : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+    private EnemyOnePatrol enemyPatrol;
+    private bool isShielded = false;
+    [SerializeField] private float shieldDuration;
+    private float shieldTimer;
+
+
+    //private Health playerHealth; (Eshaan)
+
+    private void Awake() {
+       enemyPatrol = GetComponentInParent<EnemyOnePatrol>();
+    }
+
+    void Update() {
+        cooldownTimer += Time.deltaTime;
+
+        if (PlayerInSight()) {
+            if (!isShielded) {
+                if (cooldownTimer >= attackCooldown) {
+                    // playerHealth.TakeDamage(damage);
+                    cooldownTimer = 0;
+                    isShielded = true;
+                }
+            } else {
+
+            }
+        }
+
+        if (enemyPatrol != null) {
+            enemyPatrol.enabled = !PlayerInSight();
+        }
+    }
+
+    private void Shield() {
+        shieldTimer += Time.deltaTime;
+
+        if (shieldTimer > shieldDuration) {
+            isShielded = !isShielded;
+        }
+    }
+
+    private bool PlayerInSight() {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
+        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 0, Vector2.left, 0, playerLayer);
+
+        if (hit.collider != null) {
+        //    playerHealth = hit.transform.GetComponent<Health done by Eshaan>();
+        }
+
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
+        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+}
